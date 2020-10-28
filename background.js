226a5bb7166699e606765483b941523cff18ff57
trahvex <trahvex@users.noreply.github.com>
2020-10-28 21:29:33 +0100

    FUNCIONA el core
    It works!

--- background.js
+++ background.js
@@ -1,6 +1,5 @@
 //Javascript que va detras del manifestjson
 
-
 // esta funcion se ejecuta al ser instalado
 chrome.runtime.onInstalled.addListener(
 
@@ -8,12 +7,13 @@ chrome.runtime.onInstalled.addListener(
 
 );
 
+var model;
 //cargar modelo al iniciar navegador
 chrome.runtime.onStartup.addListener(
     function() {
-
-
-
+        console.log("HOLAAAAAAAAa");
+        model = tf.loadLayersModel('./modelo_tfjs/model.json');
+        console.log(model);
     }
 );
 
@@ -22,21 +22,18 @@ chrome.browserAction.setBadgeBackgroundColor({color:'#FF5733'});
 
 
 
+// ############################################## FUNCIONES PARA EL MODELO ##############################################
 
-
-
-
-
-
-// Here we will save tabId, tab url, number of blocked url and its lists
-var tabsInfo = new Map();
-var dict;
+//needed for the model
+async function processModel(){
+    model = await tf.loadLayersModel('./modelo_tfjs/model.json');
+    //model.summary();
+}
+processModel();
 
 //load dictionary for preprocessing
 jQuery.getJSON("dict_url_raw.json", function(json) {
-
     dict = json
-
     //al caracter que tiene el 0 asignado como traduccion se lo cambiamos para que no interfiera con el padding, se le da el valor de dict.length que es el immediatamente mas peque siguiente
     for (var key in dict) {
         if (dict.hasOwnProperty(key) && dict[key] == 0) {
@@ -44,6 +41,21 @@ jQuery.getJSON("dict_url_raw.json", function(json) {
         }
     }
 });
+
+
+//procesa el resultado del modelo pa retornar un int
+function processResult(data){
+    data = data.reshape([2]);
+    data = data.argMax(); //aqui tiene el valor que toca pero sigue siendo un tensor
+    return data.arraySync();
+}
+
+
+
+// Here we will save tabId, tab url, number of blocked url and its lists
+var tabsInfo = new Map();
+var dict;
+
 
 //function to create a new entry for tabsInfo
 function newInfo (tabId){
@@ -61,9 +73,9 @@ function newInfo (tabId){
 }
 
 // ############################################## REQUEST PROCESSING ##############################################
-
 chrome.webRequest.onBeforeRequest.addListener(
     function(details){ //this is a callback function executed when details of the webrequest are available
+
         const request_url = details.url;
         const idTab = details.tabId;
 
@@ -72,31 +84,31 @@ chrome.webRequest.onBeforeRequest.addListener(
         }
 
 
-        let suspicious = false;
-
+        let suspicious = 0;
 
         //######################### URL PREPROCESSING #########################
 
         //convertimos la url de string a array de caracteres
         const url_array = Array.from(request_url);
 
-        //traducimos la url de caracteres a numeros segun el diccionario creado por la notebook del theo (esta depende de la base de datos que utiliza para el training)
+        //traducimos la url de caracteres a numeros segun el diccionario creado por la notebook (esta depende de la base de datos que utiliza para el training)
         for (i=0; i < url_array.length; i++){
             if(dict.hasOwnProperty(url_array[i]))
                 url_array[i]=dict[url_array[i]];
         }
 
         //padding a la izquierda
-        padded_url_array = Array(200).fill('0').concat(url_array).slice(url_array.length);
+        padded_url_array = Array(200).fill(0).concat(url_array).slice(url_array.length);
+
 
         //######################### INFERENCE TASK #########################
+        if(model != undefined) {
+            suspicious = processResult(model.predict(tf.tensor(padded_url_array,[1, 200])));
+        }
 
+        console.log("URL:", request_url, " y es:", suspicious);
 
-
-
-
-
-
+        //return;
         if (suspicious) {
             chrome.tabs.get(idTab,
                 function(tab) {
@@ -106,8 +118,7 @@ chrome.webRequest.onBeforeRequest.addListener(
                     tabsInfo.get(idTab)[3].push(request_url);
                 }
             );
-
-            console.log(tabsInfo);
+            //console.log(tabsInfo);
             return {cancel: true};
         };
     },
